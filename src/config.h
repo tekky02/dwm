9f4a5150bc9f945cdbce177be1565593cd1fed49
tekky02 <tekky@foxmail.com>
2022-10-02 16:35:02 +0800

    fix

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=10" };
+static const char *fonts[]          = { "Source Code Pro:size=16" };
 static const char dmenufont[]       = "Source Code Pro:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -27,7 +27,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "ﳑ", "", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "ﳑ", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
